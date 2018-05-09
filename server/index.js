b27b3212a448b39d71550e0284aa45b4ae2c37d3
Ernst Pierre <pitrens.studio@gmail.com>
2018-05-09 20:36:05 +0800

    connecting app to remote mongoDB database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
 const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 5000;
+
 require('./routes/authRoutes')(server);
 require('./services/passport') // Make it execute
 
 
+mongoose.connect(keys.mongoURI);
 server.listen(PORT);
